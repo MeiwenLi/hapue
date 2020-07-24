612ed1b71bc923ee3fa1d7201fc32b7f7f279311
MeiwenLi <meiwen.li@yahoo.com>
2020-07-23 23:12:15 -0700

    Add the reporter Handler which helps send users' daily learning report.

--- src/main/java/example/ReporterHandler.java
+++ src/main/java/example/ReporterHandler.java
@@ -2,6 +2,10 @@ package example;
 
 
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.document.Attribute;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
@@ -9,6 +13,7 @@ import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,10 +37,20 @@ import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
 // Handler value: example.Handler
 public class ReporterHandler implements RequestHandler<ScheduledEvent, String> {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private static final Logger logger = LoggerFactory.getLogger(ReporterHandler.class);
+    String recordTable = "StudyRecord";
+    String userTable = "Users";
+    String userKey = "UserId"; //the primary key of talbe "Users"
+    String recordCol1 = "TextCN";
+    String recordCol2 = "TextENG";
 
     @Override
     public String handleRequest(ScheduledEvent cwevent, Context context) {
@@ -43,58 +58,110 @@ public class ReporterHandler implements RequestHandler<ScheduledEvent, String> {
             //Accessing DB
             AmazonDynamoDB dbclient = AmazonDynamoDBClientBuilder.standard()
                     .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
-                            "https://dynamodb.us-west-2.amazonaws.com","us-west-2")).build();
+                            "https://dynamodb.us-west-2.amazonaws.com", "us-west-2")).build();
 
             DynamoDB dynamoDB = new DynamoDB(dbclient);
-            Table table = dynamoDB.getTable("MailTest");
-            String userid = "meiwen.li@west.cmu.edu";
-            GetItemSpec spec = new GetItemSpec().withPrimaryKey("UserId",userid);
+            Table rTable = dynamoDB.getTable(recordTable);
+            Table uTable = dynamoDB.getTable(userTable);
 
-            try {
-                System.out.println("Attempting to read the item...");
-                Item outcome = table.getItem(spec);
-                System.out.println("GetItem succeeded: " + outcome);
-            } catch (Exception e) {
-                System.err.println("Unable to read item: " + userid);
-                System.err.println(e.getMessage());
+            List<String> userList = new ArrayList<>();
+
+            ScanRequest scanRequest = new ScanRequest()
+                    .withTableName(userTable)
+                    .withProjectionExpression(userKey);
+
+            ScanResult result = dbclient.scan(scanRequest);
+
+            //build userList to contain all userId which will be used to find their daily record and send email then
+            for (Map<String, AttributeValue> item : result.getItems()) {
+
+                userList.add(item.get(userKey).getS());
+                System.out.println("the users in Table Users are" + userKey + " AND ");
             }
-            //new mail
-            String region = "us-west-2";
-
-            String FROM = "meiwen.li@west.cmu.edu";
-            String TO = "meiwen.li@west.cmu.edu";
-            String SUBJECT = "Amazon SES test (AWS SDK for Java)";
-
-            String HTMLBODY = "<h1>Amazon SES test (AWS SDK for Java)</h1>"
-                    + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
-                    + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
-                    + "AWS SDK for Java</a>";
-            String TEXTBODY = "This email was sent through Amazon SES "
-                    + "using the AWS SDK for Java.";
-
-            AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder
-                    .standard().withRegion(Regions.US_WEST_2).build();
-
-            SendEmailRequest request = new SendEmailRequest()
-                    .withDestination(
-                            new Destination().withToAddresses(TO))
-                    .withMessage(new Message()
-                            .withBody(new Body()
-                                    .withHtml(new Content()
-                                            .withCharset("UTF-8").withData(HTMLBODY))
-                                    .withText(new Content()
-                                            .withCharset("UTF-8").withData(TEXTBODY)))
-                            .withSubject(new Content()
-                                    .withCharset("UTF-8").withData(SUBJECT)))
-                    .withSource(FROM);
-            client.sendEmail(request);
-            System.out.println("Email sent!");
 
 
+            for (String user : userList) {
+                //GetItemSpec spec = new GetItemSpec().withPrimaryKey("UserId",user);
+                System.out.println("==========we are in the loop of " + user + "\n");
+
+                Map<String, AttributeValue> expressionAttributeValues =
+                        new HashMap<String, AttributeValue>();
+
+                //get today's date for query today's record
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
+                LocalDateTime now = LocalDateTime.now();
+                ///TODO: get yesterday's time!!! We would better send record for yesterday!
+                String date = dtf.format(now);
+
+                System.out.println("Today is " + date);
+
+                String recordTime = date + "000000"; //get the earliest time for that day for later comparison
+                expressionAttributeValues.put(":userId", new AttributeValue().withS(user));
+
+                expressionAttributeValues.put(":time", new AttributeValue().withS(recordTime));
+
+                ScanRequest scanRequestr = new ScanRequest()
+                        .withTableName(recordTable)
+                        .withFilterExpression("UserId = :userId and StudyTime >= :time")
+                        .withProjectionExpression(recordCol1 + "," + recordCol2)
+                        .withExpressionAttributeValues(expressionAttributeValues);
+
+                System.out.println("the SCAN request to recordTAble is " + scanRequestr.toString() + "\n");
+
+                ScanResult resultr = dbclient.scan(scanRequestr);
+
+                System.out.println("scan request to table recorder has been completed");
+
+                if (resultr.getScannedCount() >0) {
+
+                    StringBuilder learningRecord_sb = new StringBuilder();
+
+                    for (Map<String, AttributeValue> item : resultr.getItems()) {
+                        learningRecord_sb.append(item.get(recordCol1).getS() + " => " + item.get(recordCol2).getS() + "\n");
+                        System.out.println("the record of " + recordCol1 + " is " + item.get(recordCol1).getS());
+                        System.out.println("the record of " + recordCol2 + " is " + item.get(recordCol2).getS());
+                    }
+
+                    //new mail
+                    System.out.println("the mail content should be " + learningRecord_sb.toString() + "\n");
+
+                    String region = "us-west-2";
+
+                    String FROM = "meiwen.li@west.cmu.edu";
+                    String TO = user;
+                    String SUBJECT = "Hapue Learning record" + " for " + date;
+
+                    String HTMLBODY = "<h1>YOUR HAPUE LEARNING RECORD</h1>"
+                            + "<p>"
+                            + learningRecord_sb.toString();
+
+                    String TEXTBODY = learningRecord_sb.toString();
+                    try {
+                        AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder
+                                .standard().withRegion(Regions.US_WEST_2).build();
+
+                        SendEmailRequest request = new SendEmailRequest()
+                                .withDestination(
+                                        new Destination().withToAddresses(TO))
+                                .withMessage(new Message()
+                                        .withBody(new Body()
+                                                .withHtml(new Content()
+                                                        .withCharset("UTF-8").withData(HTMLBODY))
+                                                .withText(new Content()
+                                                        .withCharset("UTF-8").withData(TEXTBODY)))
+                                        .withSubject(new Content()
+                                                .withCharset("UTF-8").withData(SUBJECT)))
+                                .withSource(FROM);
+                        client.sendEmail(request);
+                        System.out.println("Email sent!");
+                    } catch (Exception ex) {
+                        System.out.println("The email was not sent. Error message: " + ex.getMessage());
+                    }
+                }
+            }
         } catch (Exception ex) {
-            System.out.println("The email was not sent. Error message: " + ex.getMessage());
+            System.out.println("Reporter was not generated" + ex.getMessage());
         }
-
         return "Ok";
     }
 }
