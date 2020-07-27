4f4dd9f3cb4aed5494b150efd973448ebb2957ba
MeiwenLi <meiwen.li@yahoo.com>
2020-07-27 13:25:05 -0700

    add a configuration file

---
+++ src/main/java/example/Configuration.java
@@ -0,0 +1,20 @@
+package example;
+
+public class Configuration {
+    //bucket setting for uploading image to be translated, recognized, putting result and save adio
+    public static final String tans_bucket = "transbucket";
+    public static final String rek_bucket = "rekbucket";
+    public static final String text_resultBucket = "textresultbucket";
+    public static final String audio_resultBucket = "audioresultbucket";
+
+    //Tables and their fields
+    public static final String recordTable = "StudyRecord";
+    public static final String record_user = "UserId";
+    public static final String record_time = "StudyTime";
+    public static final String record_cn = "TextCN";
+    public static final String record_eng = "TextENG";
+
+    public static final String userTable = "Users";
+    public static final String user_id = "UserId";
+    public static final String user_pwd = "Password";
+}
